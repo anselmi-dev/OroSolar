96e1ac502b2fb9a316c3ab65326fb55ab698aaa9
anselmi-dev <anselmi.dev@gmail.com>
2025-12-22 13:46:11 -0300

    Logo en el mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <x-app-logo class="text-app-400 w-full max-h-full" alt="{{ config('app.name', 'Laravel') }}" />
+    <img src="{{ asset('logo/default-monochrome-black.svg') }}" class="logo" alt="{{ config('app.name', 'Laravel') }}">
 </a>
 </td>
 </tr>
